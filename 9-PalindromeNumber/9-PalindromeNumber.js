577d872e1a303c31910bdf63033308626daf4c17
Pranaya Pudasaini <158786511+Tack-12@users.noreply.github.com>
2025-09-03 21:54:07 -0500

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.js
@@ -0,0 +1,18 @@
+// Last updated: 9/3/2025, 9:54:07 PM
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let y= x.toString();
+    let z='';
+    let output = false;
+    
+    for(let i = y.length-1; i>=0 ; i--){
+        z+= y[i];
+    }
+
+    y==z ? output=true: output=false
+
+    return output;
+};
